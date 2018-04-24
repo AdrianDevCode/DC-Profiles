b87dc5cb63728a27cabd9c0c28e6482826b52f28
Adrian Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2018-04-24 11:06:02 -0400

    added showcase modal

--- students/index.js
+++ students/index.js
@@ -13,12 +13,12 @@ $(function () {
         let html = '';
 
         students.forEach((student) => {
-            html += '<div class="card">';
+            html += '<div class="card text-center">';
             html += '<img class="card-img-top" src="' + student.picture.large + '">';
             html += '<div class="card-body">';
             html += '<h5 class="card-title">' + student.fullName + '</h5><hr>';
             html += '<button data-id="' + student.id + '" type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#info">Information</button>';
-            html += '<button data-id='+ student.id +' type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#showcase">Showcase</button></div></div>';
+            html += '<button data-id=' + student.id + ' type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#showcase">Showcase</button></div></div>';
 
         });
         return html;
@@ -31,34 +31,46 @@ $(function () {
         studentInfo.forEach((student) => {
             html += '<div class="modal fade" id="info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">';
             html += '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">';
-            html += '<h5 class="modal-title" id="exampleModalLabel">' + student.firstName +'</h5>';
+            html += '<h5 class="modal-title" id="exampleModalLabel">' + student.firstName + '</h5>';
             html += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
             html += '</div><div class="modal-body"><p>' + student.fullBio + '</p><hr>';
-            html +=  '<p>' + student.missionStatement + '</p><hr><h6>' + student.email + '</h6></div>';
+            html += '<p>' + student.missionStatement + '</p><hr><h6>' + student.email + '</h6></div>';
             html += '<div class="modal-footer"><a href="' + student.githubUrl + '"><i class="fab fa-github fa-2x"></i></a>';
             html += '<a href="' + student.linkedinUrl + '"><i class="fab fa-linkedin fa-2x"></i> </a>';
-            html += '<a href="' + student.portfolioUrl + '"><i class="fas fa-suitcase fa-2x"></i> </a></div></div></div></div>'; 
-            
+            html += '<a href="' + student.portfolioUrl + '"><i class="fas fa-suitcase fa-2x"></i> </a></div></div></div></div>';
+
         });
         return html;
     }
     const renderShowcaseModal = (studentInfo) => {
         let html = '';
-        let showcases = studentInfo.showcase;
-        studentInfo.forEach((student) => {
-            html += '<div class="modal fade" id="showcase" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">';
-            html += '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">';
-            html += '<h5 class="modal-title" id="exampleModalLabel">'+ student.firstName +"'s Showcase</h5>";
-            html += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-            html += '</div><div class="modal-body">'; 
-            
-        });
-        showcases.forEach((showcase) => {
-            html += '';
+        let showcases = studentInfo["0"].showcase;
 
-        });
-        html += '</div></div></div>';
-        return html;
+        if (showcases == 0) {
+            alert("There seems to be nothing here");
+        } else {
+            studentInfo.forEach((student) => {
+                html += '<div class="modal fade" id="showcase" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">';
+                html += '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">';
+                html += '<h5 class="modal-title" id="exampleModalLabel">' + student.firstName + "'s Showcase</h5>";
+                html += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+                html += '</div><div class="modal-body">';
+
+            });
+
+            showcases.forEach((showcase) => {
+
+                html += '<div class="card text-center"><div class="card-body">';
+                html += '<h5 class="card-title">' + showcase.projectName + '</h5>';
+                html += '<p class="card-text">' + showcase.description + '</p>';
+                html += '<a href="' + showcase.githubUrl + '"><i class="fab fa-github fa-2x"></i> </a>';
+                html += '<a href="' + showcase.demoVideo + '"><i class="fab fa-youtube fa-2x"></i></a>';
+                html += '<a href="' + showcase.url + '"><i class="fas fa-code fa-2x"></i></a>';
+                html += '</div></div>';
+            });
+            html += '</div></div></div>';
+            return html;
+        }
     }
 
 
@@ -96,11 +108,11 @@ $(function () {
         $(".student-profiles").append(renderHTML);
     })();
 
-// part of search form
+    // part of search form
     const searchedName = (search) => {
         let students = [];
         dataBase.forEach((student) => {
-        let studentSearch = student.fullName.toLowerCase();
+            let studentSearch = student.fullName.toLowerCase();
             if (studentSearch.includes(search)) {
                 students.push(student);
             }
@@ -114,32 +126,32 @@ $(function () {
         let search = $("input").val().toLowerCase();
         let studentSearch = searchedName(search);
         let renderHTML = renderStudents(studentSearch);
-         $(".student-profiles").append(renderHTML);
+        $(".student-profiles").append(renderHTML);
     });
 
     const searchedId = (search) => {
         let studentInfo = [];
         dataBase.forEach((student) => {
-        let studentSearch = student.id;
+            let studentSearch = student.id;
             if (studentSearch == search) {
                 studentInfo.push(student);
             }
         });
         return studentInfo;
     }
-// had to use function() instead of () => because this would not bind to button.
-    $(".student-profiles").on("click", "button", function() { 
+    // had to use function() instead of () => because this would not bind to button.
+    $(".student-profiles").on("click", "button", function () {
         let studentId = $(this).attr("data-id");
         let dataTarget = $(this).attr("data-target");
         $(".student-modal").empty();
         let student = searchedId(studentId);
         let renderHTML;
-        if(dataTarget == "#info"){
+        if (dataTarget == "#info") {
             renderHTML = renderInformationModal(student);
-        }else {
+        } else {
             renderHTML = renderShowcaseModal(student);
         }
-         $(".student-modal").append(renderHTML);
+        $(".student-modal").append(renderHTML);
     });
 
 });
